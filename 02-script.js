3e9c62ce001bdb5976f73d240370af9530c5c7fb
Jana Jandová | Tesena <jana.jandova@tesena.com>
2023-01-11 14:57:27 +0100

    Tvorba konfliktu v souboru 02-script.js

--- 02-script.js
+++ 02-script.js
@@ -1,4 +1,5 @@
-const animals = ["dog", "cat", "turtle"]; //tvorba pole - hranaté závorky a zapíšu
+// V tomto souboru budu tvořit konflikty - která zvířata vyhrajou??
+const animals = ["dog", "cat", "mouse"]; //myš porazí slova
 
 //loopování - 3 způsoby:
 for(let i = 0; i < animals.length; i++) { // i index reprezentovaný nějakou proměnnou //i++ - navyš o 1
